7befb7876932b6ac0d244c51c9cdd615c0ebc459
Masakazu OHTSUKA <o.masakazu@gmail.com>
2015-01-26 20:52:23 +0900

    Initial

---
+++ accesslog.go
@@ -0,0 +1,95 @@
+package accesslog
+
+import (
+	"net/http"
+	"strings"
+	"time"
+)
+
+type LogRecord struct {
+	Time                                time.Time
+	Ip, Method, Uri, Protocol, Username string
+	Status                              int
+	Size                                int64
+	ElapsedTime                         time.Duration
+	CustomRecords                       map[string]string
+}
+
+type LoggingWriter struct {
+	http.ResponseWriter
+	logRecord LogRecord
+}
+
+func (r *LoggingWriter) Write(p []byte) (int, error) {
+	if r.logRecord.Status == 0 {
+		// The status will be StatusOK if WriteHeader has not been called yet
+		r.logRecord.Status = http.StatusOK
+	}
+	written, err := r.ResponseWriter.Write(p)
+	r.logRecord.Size += int64(written)
+	return written, err
+}
+
+func (r *LoggingWriter) WriteHeader(status int) {
+	r.logRecord.Status = status
+	r.ResponseWriter.WriteHeader(status)
+}
+
+// w.(accesslogger.LoggingWriter).SetCustomLogRecord("X-User-Id", "3")
+func (r *LoggingWriter) SetCustomLogRecord(key, value string) {
+	if r.logRecord.CustomRecords == nil {
+		r.logRecord.CustomRecords = map[string]string{}
+	}
+	r.logRecord.CustomRecords[key] = value
+}
+
+type Logger interface {
+	Log(record LogRecord)
+}
+
+type LoggingHandler struct {
+	handler http.Handler
+	logger  Logger
+}
+
+func NewLoggingHandler(handler http.Handler, logger Logger) http.Handler {
+	return &LoggingHandler{
+		handler: handler,
+		logger:  logger,
+	}
+}
+
+func (h *LoggingHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	ip := strings.Split(r.RemoteAddr, ":")[0]
+
+	username := "-"
+	if r.URL.User != nil {
+		if name := r.URL.User.Username(); name != "" {
+			username = name
+		}
+	}
+
+	writer := &LoggingWriter{
+		ResponseWriter: rw,
+		logRecord: LogRecord{
+			Time:        time.Time{},
+			Ip:          ip,
+			Method:      r.Method,
+			Uri:         r.RequestURI,
+			Username:    username,
+			Protocol:    r.Proto,
+			Status:      0,
+			Size:        0,
+			ElapsedTime: time.Duration(0),
+		},
+	}
+
+	startTime := time.Now()
+	h.handler.ServeHTTP(writer, r)
+	finishTime := time.Now()
+
+	writer.logRecord.Time = finishTime.UTC()
+	writer.logRecord.ElapsedTime = finishTime.Sub(startTime)
+
+	h.logger.Log(writer.logRecord)
+}
---
+++ accesslog_test.go
@@ -0,0 +1,50 @@
+package accesslog
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type customLogger struct {
+	buf string
+}
+
+func (l *customLogger) Log(record LogRecord) {
+	// l.buf += "time:" + record.Time.Format("02/Jan/2006:15:04:05 -0700")
+	// l.buf += "ip:" + record.Ip
+	l.buf += "method:" + record.Method
+	l.buf += "uri:" + record.Uri
+	l.buf += "protocol:" + record.Protocol
+	l.buf += "username:" + record.Username
+	l.buf += "status:" + fmt.Sprintf("%d", record.Status)
+	// l.buf += "elapsedTime:" + fmt.Sprintf("%d", record.ElapsedTime)
+	l.buf += "customRecords:" + fmt.Sprintf("%v", record.CustomRecords)
+}
+
+func okHandler(w http.ResponseWriter, req *http.Request) {
+	w.(*LoggingWriter).SetCustomLogRecord("x-user-id", "1")
+	w.Write([]byte(`ok`))
+}
+
+func newRequest(method, url string) *http.Request {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		panic(err)
+	}
+	return req
+}
+
+func TestOutput(t *testing.T) {
+	logger := customLogger{}
+	loggingHandler := NewLoggingHandler(http.HandlerFunc(okHandler), &logger)
+	writer := httptest.NewRecorder()
+	loggingHandler.ServeHTTP(writer, newRequest("GET", "/"))
+
+	expected := "method:GETuri:protocol:HTTP/1.1username:-status:200customRecords:map[x-user-id:1]"
+	output := logger.buf
+	if output != expected {
+		t.Errorf("expected %s but got %s", expected, output)
+	}
+}
